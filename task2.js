59cc753466a639bca608ba1cc3a1c3625563a7b6
Kamruzzaman2200 <sohel.eightb@gmail.com>
2024-02-07 22:08:11 +0600

    fix the code

--- task2.js
+++ task2.js
@@ -1,12 +1,15 @@
 function checkName(Nam) {
-    if (typeof Nam !=='string') {
-        return "Its not a name";
+    if (typeof Nam !== 'string') {
+        return "Invalid";
     }
-    var lastletter = Nam[Nam.length - 1].toLocaleLowerCase();
-    if (lastletter === 'a' || lastletter === 'y' || lastletter === 'i' || lastletter === 'e' || lastletter === 'o' || lastletter === 'u' || lastletter === 'w'||) {
-        return "Bad Name";
-    } else {
+    var lastLetter = Nam[Nam.length - 1];
+
+    if (lastLetter === 'a' || lastLetter === 'y' || lastLetter === 'i' || lastLetter === 'e' || lastLetter === 'o' || lastLetter === 'u' || lastLetter === 'w' || 
+        lastLetter === 'A' || lastLetter === 'Y' || lastLetter === 'I' || lastLetter === 'E' || lastLetter === 'O' || lastLetter === 'U' || lastLetter === 'W') {
         return "Good Name";
+    } else {
+        return "Bad Name";
     }
 }
-console.log(checkName("Jhankar"));
+
+console.log(checkName(199));
