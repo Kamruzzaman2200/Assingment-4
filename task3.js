56c4bf0799696ff38a44654b70204bd7c93ec4b7
Kamruzzaman2200 <sohel.eightb@gmail.com>
2024-02-07 22:26:00 +0600

    task 3 done

---
+++ task3.js
@@ -0,0 +1,20 @@
+function deleteInvalids(arr) {
+    if (!Array.isArray(arr)) {
+        return "Invalid Array";
+    }
+    else{
+        var validValues = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number' && !isNaN(arr[i])) {
+            validValues.push(arr[i]);
+        }
+    }
+
+    return validValues;
+    }
+}
+console.log(deleteInvalids([1, null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }])); // Output: [1, 18, -19]
+console.log(deleteInvalids(["1", {num: 2}, NaN])); // Output: []
+console.log(deleteInvalids([1, 2, -3])); // Output: [1, 2, -3]
+console.log(deleteInvalids({num: [1, 2, 3]})); // Output: "Invalid Array"
+
